ccea03bbf9a2768504edd05d28d02ec8c1f20a6f
sFarkhod <93736788+sFarkhod@users.noreply.github.com>
2022-06-23 15:14:09 +0500

    init: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    var digit, result = 0
+    while( n ){
+        digit = n % 10  
+        result = (result * 10) + digit  
+        n = Math.floor(n/10|0)  
+    }  
+
+return Math.abs(result) 
 }
+
+
+
+
+
